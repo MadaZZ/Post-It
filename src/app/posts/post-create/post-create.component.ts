387b3d9eb2e3329f1535093d002140f01c0161cc
MadaZZ <sparshranaazz@gmail.com>
2019-11-29 21:46:25 +0530

    Added angular structure
    Added base angular structure.

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Post } from '../posts.model';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
+})
+export class PostCreateComponent implements OnInit {
+
+  enteredContent = '';
+  enteredTitle = '';
+  @Output() postCreated = new EventEmitter<Post>();
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onAddPost(form: NgForm) {
+    const post: Post = {
+      title: form.value.title,
+      content: form.value.content
+    };
+    this.postCreated.emit(post);
+  }
+}
---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Post } from '../posts.model';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent implements OnInit {
+  // posts = [
+  //   {title: 'Hello Header q1', content: 'Dummy content Dummy content Dummy content Dummy content Dummy content'},
+  //   {title: 'Hello Header q2', content: 'Dummy content Dummy content Dummy content Dummy content Dummy content'},
+  //   {title: 'Hello Header q3', content: 'Dummy content Dummy content Dummy content Dummy content Dummy content'},
+  //   {title: 'Hello Header q4', content: 'Dummy content Dummy content Dummy content Dummy content Dummy content'},
+  //   {title: 'Hello Header q5', content: 'Dummy content Dummy content Dummy content Dummy content Dummy content'}
+  // ];
+  @Input() posts: Post[] = [];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
