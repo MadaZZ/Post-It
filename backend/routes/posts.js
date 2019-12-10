d3b798d483690a5e390339e3c75c51a3a3d0fcb2
MadaZZ <sparshranaazz@gmail.com>
2019-12-10 12:44:28 +0530

    Added multer - image upload backend

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,9 +1,30 @@
 const express = require('express');
-
+const multer = require('multer');
 const router = express.Router();
 
 const PostModel = require('../models/post');
 
+MIME_TYPE_MAP = {
+    'image/png': 'png',
+    'image/jpeg': 'jpg',
+    'image/jpg': 'jpg',
+};
+
+const storageConfig = multer.diskStorage({
+    destination: function (req, file, cb) {
+        const isValid = MIME_TYPE_MAP[file.mimetype];
+        let error = new Error("Invalid mime type");
+        if(isValid){
+            error = null;
+        }
+        cb(error, 'backend/imageUploads')
+    },
+    filename: function (req, file, cb) {
+        const name = file.originalname.toLowerCase().split(' ').join('-');
+        const ext = MIME_TYPE_MAP[file.mimetype];
+        cb(null, name + '-' + Date.now() + '.' + ext);
+    }
+})
 
 router.get("", (req, res, next) => {
     PostModel.find()
@@ -25,7 +46,7 @@ router.get("/:id" , (req, res, next) => {
     });
 });
 
-router.post("",(req, res, next) => {
+router.post("", multer({storage: storageConfig}).single("image"), (req, res, next) => {
     const post = new PostModel({
         title: req.body.title,
         content: req.body.content
