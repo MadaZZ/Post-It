a37c4b9e4e7db5a863b2bbc77b1093849f134d7b
MadaZZ <sparshranaazz@gmail.com>
2019-12-19 23:19:16 +0530

    Login Backend (Creating token)

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,8 @@
 const express = require('express');
 
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
 const userModel = require('../models/user');
 const router = express.Router();
 
@@ -29,6 +31,41 @@ router.post("/signup", (req, res, next) => {
         console.log(err);
     });
 
+});
+
+router.post("/login", (req, res, next) => {
+    let fetchedUser;
+    userModel.findOne({ email: req.body.email })
+    .then( user => {
+        if(!user){
+            return res.status(401).json({
+                message: 'Auth failed'
+            });
+        }
+        fetchedUser = user;
+        return bcrypt.compare(req.body.password, user.password);
+    })
+    .then(result => {
+        if(!result){
+            return res.status(401).json({
+                message: 'Auth failed'
+            });
+        }
+
+        const token = jwt.sign(
+            { email: fetchedUser.email, userId: fetchedUser._id },
+            'secret_this_should_be_longer',
+            { expiresIn: '1h' }
+        );
+        res.status(201).json({
+            token: token
+        })
+    }) 
+    .catch(err => {
+        return res.status(401).json({
+            message: 'Auth failed'
+        });
+    });
 });
 
 module.exports = router;
