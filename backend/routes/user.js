25268df4c9129fb1ad3012651866f655b13197c5
MadaZZ <sparshranaazz@gmail.com>
2019-12-19 09:47:18 +0530

    Backend for signup (With bcrypt)

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,7 +1,31 @@
 const express = require('express');
+
+const bcrypt = require('bcrypt');
+
 const router = express.Router();
 
 router.post("/signup", (req, res, next) => {
+    bcrypt.hash(req.body.password, 10)
+    .then((hash) => {
+        const user = new  user({
+            email: req.body.email,
+            password: hash
+        });
+        
+        user.save()
+        .then( (result) => {
+            res.status(201).json({
+                message: 'User Created',
+                result: result
+            });
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            });
+        });
+    
+    });
 
 });
 
