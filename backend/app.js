25ccd8b4ea16bf78c9c61dd8fb41582042027a3b
MadaZZ <sparshranaazz@gmail.com>
2019-12-28 17:50:29 +0530

    Line miss

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,6 @@ const postAddress = '/api/posts';
 const userAddress = '/api/user';
 const app = express();
 
-mongoose.connect("mongodb+srv://max:4iFFoB292kwe94h6@cluster0-9wlmc.mongodb.net/Post-it?retryWrites=true&w=majority", { useUnifiedTopology: true, useNewUrlParser: true })
 mongoose.connect("mongodb+srv://max:" + process.env.MONGO_ATLAS_PW + "@cluster0-9wlmc.mongodb.net/Post-it?retryWrites=true&w=majority", { useUnifiedTopology: true, useNewUrlParser: true })
 .then(() => {
     console.log('Connected to DB')
